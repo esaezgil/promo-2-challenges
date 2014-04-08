581de19303eebcdc8ea5ced7fc400b29c3e9d2bf
tdeschamps <thomasdscmps@gmail.com>
2014-04-08 14:08:05 +0200

    Who the hell is the Yellow King

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -4,6 +4,8 @@ def ask_and_get(param)
   #TODO:
   #- Ask the user to write a value for param
   #- Get and return the string inputted by the user
+  puts "What's your " + param
+  gets.chomp.to_s
 end
 
 def name_from_terminal
@@ -18,6 +20,6 @@ full_name = name_from_terminal
 
 #TODO: use string concatenation to build a custom message embedding the computed full_name
 
-# custom_message =
+custom_message = "Your name is: " + full_name + "?\nAre you the Yellow King?"
 
 puts custom_message
