2ad1d75ae36d2910c26db91bbb66016412b92e72
tdeschamps <thomasdscmps@gmail.com>
2014-04-09 12:04:01 +0200

    Recursive sucks

--- 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
@@ -1,15 +1,40 @@
-def sum_with_while(min,max)
-  #TODO: return the sum of the integers between min and max
+def sum_with_while(a,b)
+  #TODO: return the sum of the integers between a and b
   #CONSTRAINT: you should use a while..end structure
+  sum = 0
+
+  while a <= b
+    sum += a
+    a += 1
+  end
+
+  sum
+
 end
 
-def sum_with_for(min,max)
-  #TODO: return the sum of the integers between min and max
+def sum_with_for(a,b)
+  #TODO: return the sum of the integers between a and b
   #CONSTRAINT: you should use a for..end structure
+  sum  = 0
+  for i in a..b
+    sum += i
+  end
+
+  sum
+
 end
 
-def sum_recursive(min,max)
-  #TODO (optional): return the sum of the integers between min and max
+def sum_recursive(a,b)
+  #TODO (optional): return the sum of the integers between a and b
   #CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
+    if a > b
+      raise ArgumentError
+    end
+
+    if a < b
+      b + sum_recursive(a, b - 1)
+    else
+      a
+    end
 end
 
