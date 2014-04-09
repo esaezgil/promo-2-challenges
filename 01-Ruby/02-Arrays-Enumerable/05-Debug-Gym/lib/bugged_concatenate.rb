17fb4619fd94a8f6953aa7262e6e9f9f0b2213f5
tdeschamps <thomasdscmps@gmail.com>
2014-04-09 16:41:21 +0200

    He's watching you

--- 01-Ruby/02-Arrays-Enumerable/05-Debug-Gym/lib/bugged_concatenate.rb
+++ 01-Ruby/02-Arrays-Enumerable/05-Debug-Gym/lib/bugged_concatenate.rb
@@ -1,7 +1,9 @@
 def bugged_concatenate(array)
-  array.inject("") { |output, element| output + element }.upcase
+  array.inject(""){ |result, element| (result.to_s + element.to_s).upcase }
 end
 
 def build_1984_title
-  bugged_concatenate(1, "9", 84, " ", "George Orwell")
+  bugged_concatenate([1, 9, 84, " ", "George Orwell"])
 end
+
+puts build_1984_title
