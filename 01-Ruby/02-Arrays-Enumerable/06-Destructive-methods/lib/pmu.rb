82f25b14ff1633cf388c76393c9e89753806453b
tdeschamps <thomasdscmps@gmail.com>
2014-04-09 17:04:20 +0200

    Avec la casaque rouge

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,7 +1,7 @@
 def pmu_format!(race_array)
   #TODO: modify the given array so that it is PMU-consistent
+  race_array.sort!.reverse!.map!{|poney| "#{race_array.length - race_array.index(poney)}-" + poney + "!"}
 end
-
 
 
 
