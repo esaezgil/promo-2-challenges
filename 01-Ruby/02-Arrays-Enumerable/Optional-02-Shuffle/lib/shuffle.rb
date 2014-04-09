5c206c8ef44e701cf54d756a3ec77f2c2597b862
tdeschamps <thomasdscmps@gmail.com>
2014-04-09 17:17:49 +0200

    Should be good

--- 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/lib/shuffle.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/lib/shuffle.rb
@@ -1,3 +1,14 @@
 def shuffle(array)
   #TODO: shuffle array manually
+    subset = []
+    a_temporary_array = array
+    array_size = array.length
+
+  for i in 0..array_size - 1
+    jeffrey = rand(array.length)
+    subset << array[jeffrey]
+    array.delete_at(jeffrey)
+  end
+
+  subset
 end
