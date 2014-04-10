3553f8e4be36494db998575ba59e26c58f98edc7
tdeschamps <thomasdscmps@gmail.com>
2014-04-10 11:36:30 +0200

    Shaken or Stirred

--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,13 @@
+
+nb_bottles = ARGV[0].to_i
+
 def beersong(nb_at_start)
   #TODO: sing the song
+  until nb_at_start == 0
+      puts "#{nb_at_start} bottle#{'s' if nb_at_start != 1} of beer on the wall, #{nb_at_start} bottle#{'s' if nb_at_start != 1} of beer!"
+    nb_at_start -= 1
+  end
+  puts "Take one down, pass it around, no more bottles of beer on the wall!"
 end
+
+beersong(nb_bottles)
