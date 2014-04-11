a920646fd53d0c509e5d49707435c19a040d402f
tdeschamps <thomasdscmps@gmail.com>
2014-04-11 12:57:17 +0200

    Ternary is tricky

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,10 @@
 def array_to_hash(array)
   #TODO: implement the method :)
+  hash = {}
+
+  array.each_with_index do |elts, index|
+    key = block_given? ? (yield(index)) : (index)
+    hash[key] = elts
+  end
+  hash
 end
