63f07e6db2cd53f69267055a010e0d7e62d42e51
tdeschamps <thomasdscmps@gmail.com>
2014-04-11 15:27:17 +0200

    C.R.E.A.M

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,7 +1,19 @@
-def group_anagrams(words)
+#def group_anagrams(words)
   #TODO: group anagrams
-end
+ # subwords = words
+  #subwords.sort! {|x,y| x.chars.sort.join <=> y.chars.sort.join}
 
+#subwords.group_by {|x| x}.values.chars.sort.join
+ # subwords
+#end
+def group_anagrams(words)
+  hashwords = {}
+
+  words.each {|word| hashwords[word] = word.chars.sort.join}
+  subwords = words.sort.group_by {|word| hashwords[word]}.values
+
+  subwords
+end
 # input:
 p group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
 
