44c378500b0bb292578cc180d5d77837e7480f48
tdeschamps <thomasdscmps@gmail.com>
2014-04-14 11:16:01 +0200

    Regex de m[\*\ \-]

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,8 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  if phone_number.to_s.match(/^(0|\+? ?33 ?)[1-9]{1}([ -_]?\d{2}){4}/) != nil
+    true
+  else
+    false
+  end
 end
