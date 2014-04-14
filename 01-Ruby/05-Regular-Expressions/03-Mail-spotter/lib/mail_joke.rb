f5f7e426f74c1a1385ecbba6a7d76e9ea76d698a
tdeschamps <thomasdscmps@gmail.com>
2014-04-14 15:17:30 +0200

    michelmichel@caramail.com

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -2,4 +2,19 @@
 
 def mail_joke(email)
   # TODO: Return a joke suited to the email provided
+  surname = email.match(/^\w+([-_]?\w+)+/).to_s
+  name = email.match(/([.]\w+)/).to_s.match(/(\w+)/).to_s
+  domain = email.match(/([@]\w+[.]\w+)/).to_s.match(/(\w+[.]\w+)/).to_s
+
+  raise ArgumentError if email.match(/^(\w+[-_.]?)+[@](\w+[-_]?)[.]\w+/) == nil
+  case domain
+  when 'lewagon.org'
+    "Well done #{surname}, you're skilled and capable"
+  when 'gmail.com'
+    "#{surname}, you're an average but modern person"
+  when 'live.com'
+    "#{surname} #{name}, aren't you born before 1973?"
+  else
+    "Sorry #{surname}, we don't know how to judge '#{domain}'"
+  end
 end
