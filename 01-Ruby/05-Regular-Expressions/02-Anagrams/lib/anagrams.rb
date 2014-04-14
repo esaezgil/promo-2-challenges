a1eb30b98d8ecf42afe146ca6db1d2af7c0a6937
tdeschamps <thomasdscmps@gmail.com>
2014-04-14 13:02:46 +0200

    Hohoho

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,30 @@
 def anagrams?(a_string, another_string)
-  #TODO: implement the obvious method to test if two words are anagrams
+  #TODO: implement the obvious method to test if two words are anagram
+  if a_string.downcase.chars.sort.join.slice(/\w+/).match(/(#{another_string.downcase.chars.sort.join.slice(/\w+/)})/i) != nil
+    true
+  else
+    false
+  end
 end
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
+    hash_test = {}
+
+    test_array =[a_string, another_string]
+    b=0
+
+    test_array.each do |string|
+      string.downcase.chars.each do |letter|
+        hash_test[letter] == nil ? hash_test[letter] = 1 : hash_test[letter] += 1
+      end
+    end
+
+    hash_test.each do |key, value|
+        if key =~ /[a-z]/ &&  value % 2 != 0
+           b += 1
+        end
+    end
+
+    b < 1
 end
-  
--- 01-Ruby/05-Regular-Expressions/02-Anagrams/spec/anagrams_spec.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/spec/anagrams_spec.rb
@@ -1,10 +1,12 @@
 # Encoding: utf-8
 require "spec_helper"
 require "anagrams"
+require "pry"
 
 describe "#anagrams?" do
 
   it "should return true if words are anagrams" do
+    #binding.pry
     anagrams?("Monica Lewinsky!", "Nice silky woman").must_equal true
   end
 
@@ -18,7 +20,7 @@ describe "#anagrams_on_steroids?" do
   it "should return true if words are anagrams" do
     anagrams_on_steroids?("Monica Lewinsky!", "Nice silky woman").must_equal true
   end
-  
+
   it "should return false if words are not anagrams" do
     anagrams_on_steroids?("bill", "bob").must_equal false
   end
