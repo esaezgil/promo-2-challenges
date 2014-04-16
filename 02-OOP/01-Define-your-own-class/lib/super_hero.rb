8df445270ead230ea54994d4460de709f6625c0a
tdeschamps <thomasdscmps@gmail.com>
2014-04-16 11:37:55 +0200

    Superhero corp

---
+++ 02-OOP/01-Define-your-own-class/lib/super_hero.rb
@@ -0,0 +1,31 @@
+class SuperHero
+  attr_reader :first_name, :last_name, :superhero_name
+  attr_accessor :super_powers
+
+  def initialize(first_name, last_name, superhero_name, super_powers)
+    @first_name = first_name
+    @last_name = last_name
+    @superhero_name = superhero_name
+    @super_powers = super_powers
+  end
+
+  def changing_in_a_photo_booth
+    if @superhero_name == "Superman"
+      "I've just changed in a photo booth and nobody took the pics"
+    else
+      "I always change at home"
+    end
+  end
+
+  def banging_lois_lane
+    puts "Am I dressed as Superman? Answer by yes or no"
+
+    answer = gets.chomp
+    response = answer.strip.downcase == "yes" ? "Shit, I got to get rid of this stupid costume" : "You gonna get Supernailed"
+  end
+end
+
+superman = SuperHero.new("Clark", "Kent", "Superman", ["Flying", "Super Force", "Super Fast", "Red briefs"])
+p superman
+p superman.changing_in_a_photo_booth
+p superman.banging_lois_lane
