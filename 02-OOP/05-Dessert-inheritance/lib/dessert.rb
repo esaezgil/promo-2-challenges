417af6bf9257a5c52cb08c67f86e649ae1bb0f55
tdeschamps <thomasdscmps@gmail.com>
2014-04-17 14:30:53 +0200

    Setter and Getter?

--- 02-OOP/05-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/05-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,31 @@
 class Dessert
+  attr_accessor :name, :calories
+
   def initialize(name, calories)
-  end 
-  
-  def healthy?
+    @name = name
+    @calories = calories
   end
-  
+
+  def healthy?
+    @calories < 200
+  end
+
   def delicious?
+    true
   end
 end
 
-class JellyBean < Dessert  
-  def initialize(name, calories, flavor)
+class JellyBean < Dessert
+
+  attr_reader :flavor
+
+  def delicious?
+    @flavor != "black licorice"
   end
+
+  def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
+  end
+
 end
