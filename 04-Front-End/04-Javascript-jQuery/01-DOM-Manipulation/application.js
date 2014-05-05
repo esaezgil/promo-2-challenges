aa817ab1aa927064cca35d763f7b43f8477c9135
tdeschamps <thomasdscmps@gmail.com>
2014-05-05 13:05:25 +0200

    Disco Disco hum hum

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -9,47 +9,52 @@ function run_challenges() {
 
   // Ex 1. Read the content of the email input
   var email = null;  // TODO: replace null, keep the email variable.
-
+  var email = $('input[type=text]').val();
 
 
   // Ex 2. Fill the content of the email input with your email
 
-
+  $('input[type=text]').val('thomasdscmps@gmail.com');
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
 
+  $('#email-hint').html('<strong>This is my email now</strong>');
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
 
-
+  $('table th').addClass("blue");
 
   // Ex 5. Count the number of table body rows there are (team count!)
   var team_count = 0;  // TODO: replace 0, keep the team_count variable.
-
+  team_count = $('table tbody tr').length;
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
-
+   $newteam = $("<tr><td>15</td><td>New team</td><td>0</td></tr>");
+   $('table tbody').append($newteam);
 
   // Ex 7. Write some code to sum all points given to all teams
-  var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
-
+    var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
+    var team_count2 = $('table tbody tr').length;
+    var sumTeams = function() { for (var i=1; i < team_count2; i++) {
+        sum += parseFloat($('table tbody tr td:nth-child(3)')[i-1].innerText);
+    }};
+    sumTeams();
 
 
   // Ex 8. Change the background color of all table header cells to #DDF4FF
-
-
-
-  // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
-
+    $('thead tr th').css({background: "#DDF4FF"});
+//
+//
+  //// Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
+  $('#results').animate({"top": "+=80px", left: "+=80px"},"slow");
 
 
   // Ex 10. Remove the "Email:" label from the DOM
 
-
+  $('label').remove();
 
   // Checking exercise answers. DO NOT MODIFY BELOW
   assert_equal(email, "boris@lewagon.org");
